6ad8b2b716e0fea9707b610bd87c714d65b107e9
Alejandro Puente <alejandro@glooko.com>
2016-09-16 14:38:00 -0700

    first commit

---
+++ NimbleRide/NimbleRide/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  NimbleRide
+//
+//  Created by Alejandro Puente on 9/16/16.
+//  Copyright © 2016 Alejandro Puente. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
